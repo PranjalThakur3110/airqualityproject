477fb591e7a3c3f0a92572a1b4e0db15036e5a96
Pranjal Thakur <pranjal.1923ec1132@kiet.edu>
2022-10-04 00:43:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import requests
+import json
+
+#https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+key="38cfca025d0b0175d4fdb12591ce1640"
+cityname=input("enter city name: ")
+main=Tk()
+main.title('WEATHER REPORT')
+main.iconbitmap('th.ico')
+main.geometry("480x320")
+try:
+    api_request=requests.get("https://api.openweathermap.org/data/2.5/weather?q="+cityname +"&appid="+key)
+    api=json.loads(api_request.content)
+
+except Exception as E:
+    api='ERROR....'
+
+
+
+label=Label(main,text=api).pack()    
+
+
+
+
+
+
+
+main.mainloop()
